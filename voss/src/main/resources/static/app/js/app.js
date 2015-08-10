ec3ced3b23cc03f42902bdec1e5be07a426614fe
himanshu <tiwahim@MUUWDP945.ad.issgovernance.com>
2015-08-10 17:52:34 +0530

    structure change

---
+++ voss/src/main/resources/static/app/js/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('voss', ['ui.bootstrap', 'dialogs.main','pascalprecht.translate','dialogs.default-translations','ngRoute', 'uiSlider','ngSanitize','xeditable','ng.shims.placeholder','angularSpinner']);
+
+app.config(['$routeProvider', function($routeProvider) {
+	  $routeProvider.when('/welcome', {
+	    controller : 'PageController',
+	    templateUrl : './app/partials/sf.html'
+	  })
+	  .otherwise({
+	    redirectTo : '/'
+	  });
+	}]);
---
+++ voss/src/main/resources/static/app/js/controller.js
@@ -0,0 +1,12 @@
+app.controller('PageController', ['$scope', '$route', '$routeParams','$http','$location','$rootScope', 
+                                  function($scope, $route, $routeParams,$http,$location,$rootScope) 
+{
+       
+       init($scope, $route, $routeParams,$http,$rootScope);
+       function init($scope, $route, $routeParams,$http,$rootScope) 
+       {
+       
+       }
+}
+]);
+
