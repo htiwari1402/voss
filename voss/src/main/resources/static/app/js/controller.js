2396da0aa76039e1cada79a0c487728e84334703
htiwari1402 <htiwari1402@gmail.com>
2015-08-18 03:22:57 +0530

    hjgkhg

--- voss/src/main/resources/static/app/js/controller.js
+++ voss/src/main/resources/static/app/js/controller.js
@@ -21,14 +21,18 @@ app.controller('UserRegController', ['$scope', '$route', '$routeParams','$http',
        $scope.addUsers = function()
        {
        var userInfo = {
-    		                			userName: $scope.userName,
-    		                			name: $scope.name,
-    		                			designation: $scope.designation,
-    		                			reportingManager: $scope.reportingManager
+    		                			"userName": $scope.userName,
+    		                			"name": $scope.name,
+    		                			"designation": $scope.designation,
+    		                			"reportingManager": $scope.reportingManager
        								} ;
        $http.post("/addUsers", userInfo).
        then(function(data)
     		   {
+    	            $scope.userName = "";
+    	            $scope.name = "";
+    	            $scope.designation = "";
+    	            $scope.reportingManager = "";
     	   			console.log("Users Added");
     		   });
        }
